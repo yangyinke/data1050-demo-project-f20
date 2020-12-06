f5fcc211872073350546f705b382fe7d51f4bc8a
yangyinke <yangyin_ke@brown.edu>
2020-12-06 06:14:24 +0000

    commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# stock price under pandemic
-> DATA1050 Final Project (Yangyin Ke, Huaqi Nie, Enmin Zhou)
-
-
-Project Outline: https://docs.google.com/document/d/18sS71oL4F6kYy0TrBQouTOuEHrabHRvh6OlWyAs3b_U/edit?ts=5fb0d797
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 from pymongo import MongoClient
 import pandas as pd
 
-from database import fetch_all_bpa_as_df
+#from database import fetch_all_bpa_as_df
 
 # Definitions of constants. This projects uses extra CSS stylesheet at `./assets/style.css`
 COLORS = ['rgb(637,657,687)', 'rgb(80,80,80)', 'rgb(100,100,100)', 'rgb(115,115,115)', 'rgb(135,67,69)',
@@ -44,10 +44,9 @@ def description():
 
         
         ### Data Source
-        This project utilizes up to date covid case data from [BPA 
-        Balancing Authority](https://www.bpa.gov/news/AboutUs/Pages/default.aspx).
-        The [data source](https://transmission.bpa.gov/business/operations/Wind/baltwg.aspx) 
-        **updates every day**. 
+        This project utilizes up-to-date covid case data from [The COVID Tracking Project](https://covidtracking.com/data/national/cases)
+        and stock price data from [Yahoo Finance](https://finance.yahoo.com/). 
+        All data in the database are **updated every day**. 
         ''', className='eleven columns', style={'paddingLeft': '5%'})], className="row")
 
 def stock_price_trend():
@@ -71,36 +70,6 @@ def stock_price_fluc():
         The plots below are about stock price fluctuation and covid case number. Stocks were divided into three 
         plots based on their types of fluctuation rate
         ''', className='eleven columns', style={'paddingLeft': '5%'})], className="row")
-
-
-# def static_stacked_trend_graph(stack=False):
-#     """
-#     Returns scatter line plot of all power sources and power load.
-#     If `stack` is `True`, the 4 power sources are stacked together to show the overall power
-#     production.
-#     """
-#     df = fetch_all_bpa_as_df()
-#     if df is None:
-#         return go.Figure()
-#     sources = ['Wind', 'Hydro', 'Fossil/Biomass', 'Nuclear']
-#     x = df['Datetime']
-#     fig = go.Figure()
-#     for i, s in enumerate(sources):
-#         fig.add_trace(go.Scatter(x=x, y=df[s], mode='lines', name=s,
-#                                  line={'width': 2, 'color': COLORS[i]},
-#                                  stackgroup='stack' if stack else None))
-#     fig.add_trace(go.Scatter(x=x, y=df['Load'], mode='lines', name='Load',
-#                              line={'width': 2, 'color': 'orange'}))
-#     title = 'Energy Production & Consumption under BPA Balancing Authority'
-#     if stack:
-#         title += ' [Stacked]'
-#     fig.update_layout(template='plotly_dark',
-#                       title=title,
-#                       plot_bgcolor='#23272c',
-#                       paper_bgcolor='#23272c',
-#                       yaxis_title='MW',
-#                       xaxis_title='Date/Time')
-#     return fig
 
 def get_covid_case():
     # get covid data from MongoDB
@@ -218,74 +187,74 @@ def co_fluc_plot(covid_df, stock_df):
     fig.update_layout(title='Crude Oil Jan 21 fluctuation & number of new cases')
     return fig
 
-def what_if_description():
-    """
-    Returns description of "What-If" - the interactive component
-    """
-    return html.Div(children=[
-        dcc.Markdown('''
-        # " What If "
-        So far, BPA has been relying on hydro power to balance the demand and supply of power. 
-        Could our city survive an outage of hydro power and use up-scaled wind power as an
-        alternative? Find below **what would happen with 2.5x wind power and no hydro power at 
-        all**.   
-        Feel free to try out more combinations with the sliders. For the clarity of demo code,
-        only two sliders are included here. A fully-functioning What-If tool should support
-        playing with other interesting aspects of the problem (e.g. instability of load).
-        ''', className='eleven columns', style={'paddingLeft': '5%'})
-    ], className="row")
+# def what_if_description():
+#     """
+#     Returns description of "What-If" - the interactive component
+#     """
+#     return html.Div(children=[
+#         dcc.Markdown('''
+#         # " What If "
+#         So far, BPA has been relying on hydro power to balance the demand and supply of power. 
+#         Could our city survive an outage of hydro power and use up-scaled wind power as an
+#         alternative? Find below **what would happen with 2.5x wind power and no hydro power at 
+#         all**.   
+#         Feel free to try out more combinations with the sliders. For the clarity of demo code,
+#         only two sliders are included here. A fully-functioning What-If tool should support
+#         playing with other interesting aspects of the problem (e.g. instability of load).
+#         ''', className='eleven columns', style={'paddingLeft': '5%'})
+#     ], className="row")
 
 
-def what_if_tool():
-    """
-    Returns the What-If tool as a dash `html.Div`. The view is a 8:3 division between
-    demand-supply plot and rescale sliders.
-    """
-    return html.Div(children=[
-        html.Div(children=[dcc.Graph(id='what-if-figure')], className='nine columns'),
+# def what_if_tool():
+#     """
+#     Returns the What-If tool as a dash `html.Div`. The view is a 8:3 division between
+#     demand-supply plot and rescale sliders.
+#     """
+#     return html.Div(children=[
+#         html.Div(children=[dcc.Graph(id='what-if-figure')], className='nine columns'),
 
-        html.Div(children=[
-            html.H5("Rescale Power Supply", style={'marginTop': '2rem'}),
-            html.Div(children=[
-                dcc.Slider(id='wind-scale-slider', min=0, max=4, step=0.1, value=2.5, className='row',
-                           marks={x: str(x) for x in np.arange(0, 4.1, 1)})
-            ], style={'marginTop': '5rem'}),
+#         html.Div(children=[
+#             html.H5("Rescale Power Supply", style={'marginTop': '2rem'}),
+#             html.Div(children=[
+#                 dcc.Slider(id='wind-scale-slider', min=0, max=4, step=0.1, value=2.5, className='row',
+#                            marks={x: str(x) for x in np.arange(0, 4.1, 1)})
+#             ], style={'marginTop': '5rem'}),
 
-            html.Div(id='wind-scale-text', style={'marginTop': '1rem'}),
+#             html.Div(id='wind-scale-text', style={'marginTop': '1rem'}),
 
-            html.Div(children=[
-                dcc.Slider(id='hydro-scale-slider', min=0, max=4, step=0.1, value=0,
-                           className='row', marks={x: str(x) for x in np.arange(0, 4.1, 1)})
-            ], style={'marginTop': '3rem'}),
-            html.Div(id='hydro-scale-text', style={'marginTop': '1rem'}),
-        ], className='three columns', style={'marginLeft': 5, 'marginTop': '10%'}),
-    ], className='row eleven columns')
+#             html.Div(children=[
+#                 dcc.Slider(id='hydro-scale-slider', min=0, max=4, step=0.1, value=0,
+#                            className='row', marks={x: str(x) for x in np.arange(0, 4.1, 1)})
+#             ], style={'marginTop': '3rem'}),
+#             html.Div(id='hydro-scale-text', style={'marginTop': '1rem'}),
+#         ], className='three columns', style={'marginLeft': 5, 'marginTop': '10%'}),
+#     ], className='row eleven columns')
 
 
-def architecture_summary():
-    """
-    Returns the text and image of architecture summary of the project.
-    """
-    return html.Div(children=[
-        dcc.Markdown('''
-            # Project Architecture
-            This project uses MongoDB as the database. All data acquired are stored in raw form to the
-            database (with de-duplication). An abstract layer is built in `database.py` so all queries
-            can be done via function call. For a more complicated app, the layer will also be
-            responsible for schema consistency. A `plot.ly` & `dash` app is serving this web page
-            through. Actions on responsive components on the page is redirected to `app.py` which will
-            then update certain components on the page.  
-        ''', className='row eleven columns', style={'paddingLeft': '5%'}),
+# def architecture_summary():
+#     """
+#     Returns the text and image of architecture summary of the project.
+#     """
+#     return html.Div(children=[
+#         dcc.Markdown('''
+#             # Project Architecture
+#             This project uses MongoDB as the database. All data acquired are stored in raw form to the
+#             database (with de-duplication). An abstract layer is built in `database.py` so all queries
+#             can be done via function call. For a more complicated app, the layer will also be
+#             responsible for schema consistency. A `plot.ly` & `dash` app is serving this web page
+#             through. Actions on responsive components on the page is redirected to `app.py` which will
+#             then update certain components on the page.  
+#         ''', className='row eleven columns', style={'paddingLeft': '5%'}),
 
-        html.Div(children=[
-            html.Img(src="https://docs.google.com/drawings/d/e/2PACX-1vQNerIIsLZU2zMdRhIl3ZZkDMIt7jhE_fjZ6ZxhnJ9bKe1emPcjI92lT5L7aZRYVhJgPZ7EURN0AqRh/pub?w=670&amp;h=457",
-                     className='row'),
-        ], className='row', style={'textAlign': 'center'}),
+#         html.Div(children=[
+#             html.Img(src="https://docs.google.com/drawings/d/e/2PACX-1vQNerIIsLZU2zMdRhIl3ZZkDMIt7jhE_fjZ6ZxhnJ9bKe1emPcjI92lT5L7aZRYVhJgPZ7EURN0AqRh/pub?w=670&amp;h=457",
+#                      className='row'),
+#         ], className='row', style={'textAlign': 'center'}),
 
-        dcc.Markdown('''
+#         dcc.Markdown('''
         
-        ''')
-    ], className='row')
+#         ''')
+#     ], className='row')
 
 
 # Sequentially add page components to the app's layout
@@ -308,9 +277,9 @@ def dynamic_layout():
         dcc.Graph(id='stock_fluc1', figure=stock_price_fluc_plot(covid_df, stock_df)),
         dcc.Graph(id='stock_fluc2', figure=market_index_fluc_plot(covid_df, stock_df)),
         dcc.Graph(id='stock_fluc3', figure=co_fluc_plot(covid_df, stock_df)),
-        what_if_description(),
-        what_if_tool(),
-        architecture_summary(),
+        # what_if_description(),
+        # what_if_tool(),
+        # architecture_summary(),
     ], className='row', id='content')
 
 
@@ -320,43 +289,43 @@ app.layout = dynamic_layout
 
 # Defines the dependencies of interactive components
 
-@app.callback(
-    dash.dependencies.Output('wind-scale-text', 'children'),
-    [dash.dependencies.Input('wind-scale-slider', 'value')])
-def update_wind_sacle_text(value):
-    """Changes the display text of the wind slider"""
-    return "Wind Power Scale {:.2f}x".format(value)
+# @app.callback(
+#     dash.dependencies.Output('wind-scale-text', 'children'),
+#     [dash.dependencies.Input('wind-scale-slider', 'value')])
+# def update_wind_sacle_text(value):
+#     """Changes the display text of the wind slider"""
+#     return "Wind Power Scale {:.2f}x".format(value)
 
 
-@app.callback(
-    dash.dependencies.Output('hydro-scale-text', 'children'),
-    [dash.dependencies.Input('hydro-scale-slider', 'value')])
-def update_hydro_sacle_text(value):
-    """Changes the display text of the hydro slider"""
-    return "Hydro Power Scale {:.2f}x".format(value)
+# @app.callback(
+#     dash.dependencies.Output('hydro-scale-text', 'children'),
+#     [dash.dependencies.Input('hydro-scale-slider', 'value')])
+# def update_hydro_sacle_text(value):
+#     """Changes the display text of the hydro slider"""
+#     return "Hydro Power Scale {:.2f}x".format(value)
 
 
 
-@app.callback(
-    dash.dependencies.Output('what-if-figure', 'figure'),
-    [dash.dependencies.Input('wind-scale-slider', 'value'),
-     dash.dependencies.Input('hydro-scale-slider', 'value')])
-def what_if_handler(wind, hydro):
-    """Changes the display graph of supply-demand"""
-    df = fetch_all_bpa_as_df(allow_cached=True)
-    x = df['Datetime']
-    supply = df['Wind'] * wind + df['Hydro'] * hydro + df['Fossil/Biomass'] + df['Nuclear']
-    load = df['Load']
+# @app.callback(
+#     dash.dependencies.Output('what-if-figure', 'figure'),
+#     [dash.dependencies.Input('wind-scale-slider', 'value'),
+#      dash.dependencies.Input('hydro-scale-slider', 'value')])
+# def what_if_handler(wind, hydro):
+#     """Changes the display graph of supply-demand"""
+#     df = fetch_all_bpa_as_df(allow_cached=True)
+#     x = df['Datetime']
+#     supply = df['Wind'] * wind + df['Hydro'] * hydro + df['Fossil/Biomass'] + df['Nuclear']
+#     load = df['Load']
 
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x, y=supply, mode='none', name='supply', line={'width': 2, 'color': 'pink'},
-                  fill='tozeroy'))
-    fig.add_trace(go.Scatter(x=x, y=load, mode='none', name='demand', line={'width': 2, 'color': 'orange'},
-                  fill='tonexty'))
-    fig.update_layout(template='plotly_dark', title='Supply/Demand after Power Scaling',
-                      plot_bgcolor='#23272c', paper_bgcolor='#23272c', yaxis_title='MW',
-                      xaxis_title='Date/Time')
-    return fig
+#     fig = go.Figure()
+#     fig.add_trace(go.Scatter(x=x, y=supply, mode='none', name='supply', line={'width': 2, 'color': 'pink'},
+#                   fill='tozeroy'))
+#     fig.add_trace(go.Scatter(x=x, y=load, mode='none', name='demand', line={'width': 2, 'color': 'orange'},
+#                   fill='tonexty'))
+#     fig.update_layout(template='plotly_dark', title='Supply/Demand after Power Scaling',
+#                       plot_bgcolor='#23272c', paper_bgcolor='#23272c', yaxis_title='MW',
+#                       xaxis_title='Date/Time')
+#     return fig
 
 
 if __name__ == '__main__':
